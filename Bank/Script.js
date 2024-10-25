964e8d2130faec5ff977f1ddc26eb1e6c50704f3
krishhackz <krishhackz.in@gmail.com>
2024-10-25 10:10:42 +0530

    Converting alert into Window.alert

--- Bank/Script.js
+++ Bank/Script.js
@@ -1,14 +1,15 @@
-alert("Welcome to Krish Bankz");
+const prompt = require("prompt-sync")();
+Window.alert("Welcome to Krish Bankz");
 var TotalAmount;
 var checkbank=prompt("Do you have a Bank Account? (YES/NO): ");
-if(checkbank==1||checkbank=="yes"||checkbank=="YES"||checkbank=="Yes"){
+if(checkbank=="yes"||checkbank=="YES"||checkbank=="Yes"){
     var account=parseInt(prompt("Enter your Account Number"));
     if(account==123){
         let user = parseInt(prompt("Enter 1:Check Balance, 2:Withdraw, 3:Deposite, 4:Transfer :"));
         let Balance = 25590;
         switch(user){
             case 1:
-                alert(`Your Balance is: ${Balance}rs`);
+                Window.alert(`Your Balance is: ${Balance}rs`);
             break;
             case 2:
                 let Withdraw = parseInt(prompt("Enter Withdraw Amount: "));
@@ -40,14 +41,14 @@ if(checkbank==1||checkbank=="yes"||checkbank=="YES"||checkbank=="Yes"){
                 }
             break;
             default:
-                alert("Please Enter a Number between 1 to 4");
+                Window.alert("Please Enter a Number between 1 to 4");
         }
     } else if(account==456){
         let user = parseInt(prompt("Enter 1:Check Balance, 2:Withdraw, 3:Deposite, 4:Transfer :"));
         let Balance = 562900;
         switch(user){
             case 1:
-                alert(`Your Balance is: ${Balance}rs`);
+                Window.alert(`Your Balance is: ${Balance}rs`);
             break;
             case 2:
                 let Withdraw = parseInt(prompt("Enter Withdraw Amount: "));
@@ -79,14 +80,14 @@ if(checkbank==1||checkbank=="yes"||checkbank=="YES"||checkbank=="Yes"){
                 }
             break;
             default:
-                alert("Please Enter a Number between 1 to 4");
+                Window.alert("Please Enter a Number between 1 to 4");
         }
     } else if(account==789){
         let user = parseInt(prompt("Enter 1:Check Balance, 2:Withdraw, 3:Deposite, 4:Transfer :"));
         let Balance = 13431;
         switch(user){
             case 1:
-                alert(`Your Balance is: ${Balance}rs`);
+                Window.alert(`Your Balance is: ${Balance}rs`);
             break;
             case 2:
                 let Withdraw = parseInt(prompt("Enter Withdraw Amount: "));
@@ -118,12 +119,12 @@ if(checkbank==1||checkbank=="yes"||checkbank=="YES"||checkbank=="Yes"){
                 }
             break;
             default:
-                alert("Please Enter a Number between 1 to 4");
+                Window.alert("Please Enter a Number between 1 to 4");
         }
     } else {
-        alert("Invalid Account Number");
+        Window.alert("Invalid Account Number");
     }
-} else if(checkbank==2||checkbank=="no"||checkbank=="NO"||checkbank=="No"){
+} else if(checkbank=="no"||checkbank=="NO"||checkbank=="No"){
     let user = parseInt(prompt("Enter 1:Create Account, 2:Exit :"));
     if(user==1){
         let name=prompt("Enter your Name: ");
@@ -133,22 +134,22 @@ if(checkbank==1||checkbank=="yes"||checkbank=="YES"||checkbank=="Yes"){
             if(DL=="Yes"||DL=="yes"||DL=="YES"){
                 let adhar=parseInt(prompt("Enter your Adhar Number: "));
                 let pan=parseInt(prompt("Enter yout PAN Number: "));
-                alert("Thanks for submitting your details, your account will be created soon");
+                Window.alert("Thankyou for submitting your details, your account will be created soon");
             } else if(DL=="No"||DL=="no"||DL=="NO"){
-                alert("Sorry, You can't create bank account. Due to you don't have Driving License");
+                Window.alert("Sorry, You can't create bank account. Due to you don't have Driving License");
             } else{
-                alert("Invalid Input of Driving License");
+                Window.alert("Invalid Input of Driving License");
             }
         } else if(age<18){
-            alert("Sorry, You can't create bank account. Due to you are minor");
+            Window.alert("Sorry, You can't create bank account. Due to you are minor");
         } else{
-            alert("Invalid Input of Age");
+            Window.alert("Invalid Input of Age");
         }
     } else if(user==2){
-        alert("Visit Again")
+        Window.alert("Visit Again")
     } else{
-        alert("Please Enter a Number between 1 to 2");
+        Window.alert("Please Enter a Number between 1 to 2");
     }
 } else{
-    alert("Please select Yes or No");
+    Window.alert("Please select Yes or No");
 }
